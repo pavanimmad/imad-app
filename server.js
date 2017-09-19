df95ab73f712418c4b43b2be604f9185a00ded48
pavanimmad <32090010+pavanimmad@users.noreply.github.com>
2017-09-19 22:52:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-1', function (req, res) {
+  res.send ( 'Article-1 files will be shortly loaded ');
+    
+});
+  app.get('/article-2', function (req, res) {
+  res.send( 'Artice -2 file will be loaded shortly ');
+});
+app.get('/article-3', function (req, res) {
+  res.send( ' Article-3 file will be loaded shortly ');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
